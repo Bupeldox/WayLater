0d85005b1f5c28467df1363ac9abbdea4324d926
John Warren <john@bluestreamacademy.com>
2023-03-24 11:32:21 +0000

    search page

--- routes/consumer.routes.js
+++ routes/consumer.routes.js
@@ -13,20 +13,17 @@ const viewfp = "pages/consumer/";
 // Add a binding to handle '/'
 router.get('/', (req, res) => {
     var latestReviews = dataHandler.getLatestReviews();
-    res.render('home', { reviews:latestReviews });
+    res.render('pages/home', { reviews:latestReviews });
 })
 
-router.get('/products/:search', (req, res) => {
-    var search = req.params.search;
+router.get('/search', (req, res) => {
+    var search = req.query.q;
 
     var filteredReviews = dataHandler.getFilteredReviews(search);
 
-    res.render(viewfp + 'reviewlist', { title: "products", reviews:filteredReviews });
-})
+    res.render(viewfp + 'reviewList', { title: "Search", search:search, reviews:filteredReviews });
+});
 
-router.get('/reviews', (req, res) => {
-    res.render(viewfp + 'reviewlist', { title: "reviews" });
-})
 
 
 
